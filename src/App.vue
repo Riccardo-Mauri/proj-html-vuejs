6cde3dd74d7656f4b83d52f3bcb3eba288b32cd1
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-09-19 15:36:30 +0200

    inizializzazione progetto/commentata ogni sezione principale

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script>
 //1- Importazione del componenente necessario che voglio aggiungere qua
-import AppHeader from './components/AppHeader.vue';
+
 
 export default {
   data() {
     return {
-      count: 0
+     
     }
   },
   //2- Dichiarazione del componente 
   components: {
-    AppHeader
+    
   },
   methods: {
-    incrementCount(){
-      this.count++;
-    }
+   
   }
 }
 </script>
@@ -23,13 +21,49 @@ export default {
 <template>
   <div>
     <!--3- Utilizzo del componenete -->
-    <AppHeader />
-   <button @click="incrementCount()">
-    {{ count }}
-   </button>
+    <header>
+      <div><!--link a sinistra Download APP/ ADS    a destra link con icone di FB/INST/X/YT--></div>
+      <div><!--logo al centro della pagina/container avada food--></div>
+      <div><!-- al centro della pagina/container link con pseudo elemento quando in hover di home/recipes/places/blog/about/contact/search--></div>
+    </header>
+   <main>
+    <section>
+      <!--jumbotron con sfondo e container all'interno con scritte  -->
+    </section>
+    <section>
+      <!--container con titolo e tre carte con immagine titolo e titoletto e alzato leggemente sopra all'altro container-->
+    </section>
+    <section>
+      <!--container titolo paragrafo e due container divisi uno a sinistra e l'altro a destra-->
+    </section>
+    <section>
+      <!--container con titolo paragrafo e un altro container con dentro 8 immagini con sfondo e titolo-->
+    </section>
+    <section>
+      <!--container con a sinistra titolo e paragrafo e a destra un buttone arancione-->
+    </section>
+    <section>
+      <!--mega section con un container diviso in due 80/20 a sinistra il container di 80% con una card principale enorme con sotto 6 cards da 2 per riga con  titolo/paragrafo/descrizione/ a sinistra readm more e a destra icona commenti e sotto una riga con load more posts
+      la seconda parte a destra di 20% del container avrà a immagine con sotto un altra immagine-descrizione laterale e sotto una hr una search bar un div con "follow us" e icone FB/ISNT/X/YT/P , sotto ancora un altro div con tre righe di icone rotonde paragrafo , infine un div con simulazione di tweets-->
+    </section>
+   </main>
+   <footer>
+    <section>
+      <!--sezione sopra con sfondo background al centro un container con titolo/paragrafo/searchbar/subscribe button/ paragrafo piccolino-->
+    </section>
+    <section>
+      <!--sezione di mezzo con solo 4 immagini tutta la width-->
+    </section>
+    <section>
+      <!--sezione inferiore diviso in 4 container il primo con logo/icone FB/INST/X/YT  il secondo con una lista di link Recipes/places/blog/about/contact il terzo con titolo e una lista con info non link il quarto con titolo e due bottoni-->
+    </section>
+    <section>
+      <!--sezione ultima inferiore con colore di sfondo diverso e solo una scritta copyright centrale-->
+    </section>
+   </footer>
   </div>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@import 'bootstrap/scss/bootstrap';
 </style>
